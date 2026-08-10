3dc38f80612786c265c8a726bbde847735e215f3
Darrel Tapilaha <tapsdarrel@gmail.com>
2026-08-10 14:27:33 -0700

    Completed chunk array by using Slice() operator

---
+++ chunkArray.js
@@ -0,0 +1,67 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function (arr, size) {
+
+    /* first way to do it by loop and conditional statements */
+//     let newArr = [];
+
+//     let tempArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (tempArr.length < size) {
+//         tempArr.push(arr[i]);
+//         if (tempArr.length === size) {
+//             newArr.push(tempArr);
+//             tempArr = [];
+//         } else if (i === arr.length - 1) {
+//             newArr.push(tempArr);
+//         }
+//         }
+//     }
+//     return newArr;
+
+    /* faster way to do it use slice() operator */
+    let chunkedArray = []
+    for (let i = 0; i < arr.length; i += size) {
+        chunkedArray.push(arr.slice(i, i + size))
+    }
+
+    return chunkedArray
+};
+
+const arr = [1,9,6,3,2]
+const size = 3
+console.log(chunk(arr, size))
+
+/*
+Given an array arr and a chunk size size, return a chunked array.
+
+A chunked array contains the original elements in arr, but consists of subarrays each of length size. The length of the last subarray may be less than size if arr.length is not evenly divisible by size.
+
+Please solve it without using lodash's _.chunk function.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,3,4,5], size = 1
+Output: [[1],[2],[3],[4],[5]]
+Explanation: The arr has been split into subarrays each with 1 element.
+Example 2:
+
+Input: arr = [1,9,6,3,2], size = 3
+Output: [[1,9,6],[3,2]]
+Explanation: The arr has been split into subarrays with 3 elements. However, only two elements are left for the 2nd subarray.
+Example 3:
+
+Input: arr = [8,5,3,2,6], size = 6
+Output: [[8,5,3,2,6]]
+Explanation: Size is greater than arr.length thus all elements are in the first subarray.
+Example 4:
+
+Input: arr = [], size = 1
+Output: []
+Explanation: There are no elements to be chunked so an empty array is returned.
+*/
